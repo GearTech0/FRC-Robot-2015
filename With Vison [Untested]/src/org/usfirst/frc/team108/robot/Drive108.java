86a1ce7dcef72a891ac955493e473b60c0772521
boxerbaby30 <boxerbaby30@gmail.com>
2015-02-05 16:02:20 -0500

    Update Drive108.java

--- With Vison [Untested]/src/org/usfirst/frc/team108/robot/Drive108.java
+++ With Vison [Untested]/src/org/usfirst/frc/team108/robot/Drive108.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class Drive108 {
-	ArrayList<Talon> talonSet = new ArrayList<>();
+	ArrayList<VictorSP> talonSet = new ArrayList<>();
 	
 	Drive108(int numTalon){
 		for(int i = 0; i < numTalon; i++)
